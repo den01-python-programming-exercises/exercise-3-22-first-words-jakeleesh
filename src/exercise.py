ca91df50e89aa3a74abb567a40e0bd3e14850d76
Jake Lee <jake.lee@fdmgroup.com>
2021-10-09 11:17:42 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    while True:
+        text = input()
+        if (text != ""):
+            text_split = text.split()
+            print(text_split[0])
+        else:
+            break
+
 
 if __name__ == '__main__':
     main()
